b3095b600b8ace9560d3f74731873a3be974bd08
alhkam <alhkam.dev@gmail.com>
2026-06-19 13:40:41 +0200

    Initial commit

---
+++ film-web/src/main/java/com/alhkam/film_web/FilmWebApplication.java
@@ -0,0 +1,12 @@
+package com.alhkam.film_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmWebApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FilmWebApplication.class, args);
+  }
+}
